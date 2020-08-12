3f812238d9a0baa976ebee5c8da8787723312f54
Yenny D <ydavidson2021@gmail.com>
2020-08-12 16:28:03 -0500

    Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
 import SweetInfo from './SweetInfoComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { SWEETS } from '../shared/sweets';
 
 
@@ -9,27 +13,26 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sweets: SWEETS,
-      selectedSweet: null
+      sweets: SWEETS
     };
   }
-
-  onSweetSelect(sweetId) {
-    this.setState({selectedSweet: sweetId});
-  }
-
   render() {
-      return (
-          <div>
-              <Navbar dark color="info">
-                <div className="container">
-                    <NavbarBrand href="/">Sweet Tooth Fairy GPS</NavbarBrand>
-                </div>
-              </Navbar>
-              <Directory sweets={this.state.sweets} onClick={sweetId => this.onSweetSelect(sweetId)}/>
-              <SweetInfo sweet={this.state.sweets.filter(sweet => sweet.id === this.state.selectedSweet)[0]} />
-          </div>
+    const HomePage = () => {
+      return(
+          <Home />
       );
+    }
+    return (
+      <div>
+          <Header/>
+          <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/directory' render={() => <Directory sweets={this.state.sweets} />} />
+              <Redirect to='/home' />
+          </Switch>
+          <Footer/>
+      </div>
+    );
   };
 }
 
