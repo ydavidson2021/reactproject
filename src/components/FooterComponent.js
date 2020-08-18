97612d567bc44ddab659896530b76f0d4a309454
Yenny D <ydavidson2021@gmail.com>
2020-08-17 19:15:21 -0500

    Home

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -20,7 +20,6 @@ function Footer(props){
                         <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
                         <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}
                         <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter" /></a>{' '}
-                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube" /></a> 
                     </div>
                     <div className="col-sm-4 text-center">
                         <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -20,29 +20,29 @@ function Home(props) {
             <div className="row">
                 <div className="col-md m-1">
                     <h1 align-self="center"> Sweet Cravings?</h1>
-                    <h2>Sweet Tooth Fairy GPS has the answer!</h2>
-                    <h2>INSERT CAROUSEL HERE</h2>
+                    <h2>Looking for sweet spots in Downtown San Antonio?</h2>
+                    <Image src="./assets/images/candy.jpg" fluid width="400" height="400" className="rounded mx-auto d-block"/>
                 </div>
             </div>
             <div className="row">
                 <div className="col-md m-1">
                     <h1 align-self="center"> Sample Treats</h1>
-                    <h2>Looking for sweet spots in Downtown San Antonio? </h2>
+                    <h2>Sweet Tooth Fairy GPS has the answer!</h2>
                 </div>
             </div>
             <div className="row">
             <div className="col-lg-4">
-            <Image src="./assets/images/macaron.jpg" width="200"  height="200" roundedCircle />
+            <Image src="./assets/images/macaron.jpg" width="300"  height="300" roundedCircle className="mx-auto d-block" />
             <h3 className="feature-title">Macarons</h3>
             </div>
 
             <div className="col-lg-4">
-            <Image src="./assets/images/churros.jpg" width="200"  height="200" roundedCircle />
+            <Image src="./assets/images/churros.jpg" width="300"  height="300" roundedCircle className="mx-auto d-block" />
             <h3 className="feature-title">Churros</h3>
             </div>
 
             <div className="col-lg-4">
-            <Image src="./assets/images/paleta.jpg" width="200"  height="200" roundedCircle />
+            <Image src="./assets/images/paleta.jpg" width="300"  height="300" roundedCircle className="mx-auto d-block" />
             <h3 className="feature-title">Paletas</h3>
 
             </div>
